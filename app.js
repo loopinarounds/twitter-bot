91a748787e192a871e62872254681253f46565f0
loopinarounds <86264161+loopinarounds@users.noreply.github.com>
2022-10-07 14:44:00 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+//Requiring our dependencies
+var Twitter = require('twitter');
+var config = require('./config.js');
+
+var T = new Twitter(config);
+
+
+//Search Query Parameters, searches for the hashtag for memes about reselling culture
+var params = {
+    q: '#resellmemes',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+  }
+
+  //Initiate your search using the above paramaters
+T.get('search/tweets', params, function(err, data, response) {
+  // If there is no error, proceed
+  if(!err){
+    // Loop through the returned tweets
+    for(let i = 0; i < data.statuses.length; i++){
+      // Get the tweet Id from the returned data
+      let id = { id: data.statuses[i].id_str }
+      // Try to Favorite the selected Tweet
+      T.post('favorites/create', id, function(err, response){
+        // If the favorite fails, log the error message
+        if(err){
+          console.log(err[0].message);
+        }
+        // If the favorite is successful, log the url of the tweet
+        else{
+          let username = response.user.screen_name;
+          let tweetId = response.id_str;
+          console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+        }
+      });
+    }
+  } else {
+    console.log(err);
+  }
+})
